82ee6f298ce5b59294518b7f442a5f8a0fc27532
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-10-04 16:35:04 +0300

    Active Model Serializer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,10 +6,30 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
-  def show
-    movie = Movie.find(params[:id])
-    render json: movie
-  end
+  # def show
+  #   movie = Movie.find(params[:id])
+  #   render json: movie
+  # end
+
+#   def show
+#   movie = Movie.find(params[:id])
+#   render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+# end
+
+def show
+  movie = Movie.find(params[:id])
+  render json: movie
+end
+
+def summary
+  movie = Movie.find(params[:id])
+  render json: movie, serializer: MovieSummarySerializer
+end
+
+def summaries
+  movies = Movie.all
+  render json: movies, each_serializer: MovieSummarySerializer
+end
 
   private
 
